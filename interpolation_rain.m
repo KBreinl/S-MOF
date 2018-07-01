0ff48a70af5b674c6c374ed318280ff4c8c512ce
Breinl <korbinian.breinl@geo.uu.se>
2018-07-01 20:45:01 +0200

    Add files via upload
    Interpolation routines (advanced)

---
+++ interpolation_rain.m
@@ -0,0 +1,52 @@
+function [interp_rain] = interpolation_rain(interp_rain,rain_D,rain_date_D,rain_date_H,id_rec,dist_rain,dist_calc)
+
+% Loops through sites without hourly recording
+for i=1:length(id_rec)
+    disp(i)
+    for ii=1:length(rain_date_D);
+        if rain_D(ii,id_rec(i))>0
+            k=find(rain_date_H==rain_date_D(ii));
+            % Build matrix with closest sites, rainfall amount and distance
+            id=sum(interp_rain(k,dist_rain(id_rec(i),:)))';
+            id(:,2)=dist_rain(id_rec(i),:);
+            id(:,3)=dist_calc(id_rec(i),:);
+            
+            % Remove sites without rainfall
+            id(id(:,1)==0,:)=[];
+            % If not enough neighbor sites duplicate the same site
+            if size(id,1)<3
+                id=repmat(id,3,1);
+            end
+            
+            if isempty(id)==0
+                % Limit to three neighboring sites
+                id=id(1:3,:);
+                % Inverse distance weighing
+                id(:,3)=1./id(:,3);
+                id(:,3)=id(:,3)/sum(id(:,3));
+                
+                % Build raw hyetograph from three sites (weighted by distance)
+                hyeto=interp_rain(k,id(1,2))*id(1,3)+interp_rain(k,id(2,2))*id(2,3)+interp_rain(k,id(3,2))*id(3,3);
+                % Calculate average duration (again weighted by distance)
+                idx=round((sum(interp_rain(k,id(1,2))>0)*id(1,3)+sum(interp_rain(k,id(2,2))>0)*id(2,3)+sum(interp_rain(k,id(3,2))>0)*id(3,3)));
+                % Cut out fraction (in sequence of overlapping hyetographs)
+                if idx>1;
+                    [a,~]=find(hyeto>0);
+                    idx_range=randi([1 length(a)-idx+1],1);
+                    idx_range=a(idx_range:idx_range+idx-1);
+                    hyeto(ismember(1:24,idx_range)==0)=0;
+                end
+                % build fragments
+                hyeto=hyeto./sum(hyeto);
+                
+                interp_rain(k,id_rec(i))=hyeto*rain_D(ii,id_rec(i));
+                
+            else
+                disp('achtung')
+                interp_rain(k,id_rec(i))=rain_D(ii,id_rec(i))/24;
+            end
+        end
+    end
+end
+
+
---
+++ interpolation_temp.m
@@ -0,0 +1,31 @@
+function [interp_temp] = interpolation_temp(interp_temp,temp_D,temp_date_D,temp_date_H,id_rec,dist_temp,dist_calc)
+
+% Loops through sites without hourly recording
+for i=1:length(id_rec)
+    disp(i)
+    for ii=1:length(temp_date_D);
+          k=find(temp_date_H==temp_date_D(ii));
+            % Build matrix with closest sites, rainfall amount and distance
+            id=sum(interp_temp(k,dist_temp(id_rec(i),:)))';
+            id(:,2)=dist_temp(id_rec(i),:);
+            id(:,3)=dist_calc(id_rec(i),:);
+            
+            % If not enough neighbor sites duplicate the same site
+            if size(id,1)<3
+                id=repmat(id,3,1);
+            end
+            % Limit to three neighboring sites
+            id=id(1:3,:);
+            % Inverse distance weighing
+            id(:,3)=1./id(:,3);
+            id(:,3)=id(:,3)/sum(id(:,3));
+            
+            % Build raw circle from three sites (weighted by distance)
+            circle_temp=interp_temp(k,id(1,2))*id(1,3)+interp_temp(k,id(2,2))*id(2,3)+interp_temp(k,id(3,2))*id(3,3);
+            % build fragments
+            circle_temp=circle_temp-mean(circle_temp);
+            circle_temp=circle_temp+temp_D(ii,id_rec(i));
+            interp_temp(k,id_rec(i))=circle_temp;
+    end
+end
+
